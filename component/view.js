c6f25930c0e7a1adb9260543f6f8c800421245de
Zeezahcodes <azeezatolatunde11@gmail.com>
2021-04-15 09:18:16 -0700

    API success message updated

--- component/view.js
+++ component/view.js
@@ -12,7 +12,7 @@ exports.getAllEntry = async (req, res) => {
 exports.createEntry = async (req, res) => {
   const newData = await model.create(req.body);
   res.status(201).json({
-    report: "Successful...",
+    report: "Record Successfully Created...",
     data: newData,
   });
 };
@@ -24,10 +24,13 @@ exports.getSingleEntry = async (req, res) => {
 
 exports.updateSingleEntry = async (req, res) => {
   const newData = await model.findByIdAndUpdate(req.params.id, req.body);
-  res.status(200).json(newData);
+  res.status(200).json({
+      report: "Update Successful...",
+      data: newData
+  });
 };
 
 exports.deleteSingleEntry = async (req, res) => {
   const newData = await model.findByIdAndDelete(req.params.id, req.body);
-  res.status(200).json("Entry has been successfully deleted");
+  res.status(200).json("Entry has been successfully deleted!");
 };
